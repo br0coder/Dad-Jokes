c1f924181569598da271974526f1c635f4ec285b
br0coder <112611863+br0coder@users.noreply.github.com>
2022-09-01 08:53:43 +0100

    Update dad_jokes.py

--- dad_jokes.py
+++ dad_jokes.py
@@ -14,7 +14,7 @@ def jokes(term):
         params={
             "term": term,
             "limit": 30})
-
+    # json requests received in dict data structure (format)
     data = r.json()
     joke_in_lst = [x["joke"] for x in data["results"]]
     total_jokes = data["total_jokes"]
@@ -22,13 +22,12 @@ def jokes(term):
     if total_jokes:
         joke = choice(joke_in_lst)
         if total_jokes > 1:
-            print(f"I've got {total_jokes} jokes. Here's one: \n{joke}")
-        # total_jokes ==1
+            print(f"\nI've got {total_jokes} jokes. Here's one: \n\n{joke}")
+        # total_jokes ==1 or total_joke == 0
         else:
-            print(f"I've got only 1 joke here it is ! :  \n{joke}")
+            print(f"\nI've got only 1 joke, here it is ! :  \n\n{joke}")
     else:
-        print(f"Sorry, I don't have jokes about {term} :(")
-
+        print(f"\nSorry, I don't have jokes about {term} :(")
 
 ascii = figlet_format("Ratio Dad Jokes")
 logo = colored(ascii, "cyan")
